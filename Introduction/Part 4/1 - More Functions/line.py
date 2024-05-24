f21c9d44f4614833c56fb56e3ec7ee4b2e9ea5eb
sujade <dnmzsu@gmail.com>
2024-05-24 16:26:29 +0200

    add daily exercises

---
+++ Introduction/Part 4/1 - More Functions/line.py
@@ -0,0 +1,7 @@
+#Please write a function named line, which takes two arguments: an integer and a string. The function prints out a line of text, the length of which is specified by the first argument. The character used to draw the line should be the first character in the second argument. If the second argument is an empty string, the line should consist of stars.
+
+def line(a, b):
+    if b:
+        print(b[0] * a)
+    else:
+        print('*' * a)
